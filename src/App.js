8442ae6d75ae8ffe1a151d2137082f5d69f1d4b5
riteshray007 <ray.ritesh007@gmail.com>
2023-07-16 21:40:57 +0530

    albumlists final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import './App.css';
 import Navbar from './Components/Navbar';
-import AlbumForm from './Components/AlbumForm';
 import AlbumList from './Components/AlbumList';
 
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-      <AlbumForm/>
       <AlbumList/>
     </div>
   );
--- src/Components/AlbumList.js
+++ src/Components/AlbumList.js
@@ -1,7 +1,86 @@
 import React from 'react'
+import AlbumForm from './AlbumForm'
 
 export default function AlbumList() {
-  return (
-    <div>AlbumList</div>
-  )
+      return (
+            <>
+                  <AlbumForm />
+
+                  <div className='albumlistmain' >
+                        <div className='listbuttons' >
+                              <h2>Your albums </h2>
+                              <button className='addalbum' > Add album </button>
+                        </div>
+                        <ul className='albumlist' >
+                              <li className='albums' >
+                                    <div>
+                                    <img  src='https://cdn-icons-png.flaticon.com/128/3342/3342207.png'  className='albumicon'  />
+                                    </div>
+                                    <h3 className='albumtitle' > ritesh </h3>
+                              </li>
+                              <li className='albums' >
+                                    <div>
+                                    <img  src='https://cdn-icons-png.flaticon.com/128/3342/3342207.png'  className='albumicon'  />
+                                    </div>
+                                    <h3 className='albumtitle' > ritesh </h3>
+                              </li>
+                              <li className='albums' >
+                                    <div>
+                                    <img  src='https://cdn-icons-png.flaticon.com/128/3342/3342207.png'  className='albumicon'  />
+                                    </div>
+                                    <h3 className='albumtitle' > ritesh </h3>
+                              </li>
+                              <li className='albums' >
+                                    <div>
+                                    <img  src='https://cdn-icons-png.flaticon.com/128/3342/3342207.png'  className='albumicon'  />
+                                    </div>
+                                    <h3 className='albumtitle' > ritesh </h3>
+                              </li>
+                              <li className='albums' >
+                                    <div>
+                                    <img  src='https://cdn-icons-png.flaticon.com/128/3342/3342207.png'  className='albumicon'  />
+                                    </div>
+                                    <h3 className='albumtitle' > ritesh </h3>
+                              </li>
+                              <li className='albums' >
+                                    <div>
+                                    <img  src='https://cdn-icons-png.flaticon.com/128/3342/3342207.png'  className='albumicon'  />
+                                    </div>
+                                    <h3 className='albumtitle' > ritesh </h3>
+                              </li>
+                              <li className='albums' >
+                                    <div>
+                                    <img  src='https://cdn-icons-png.flaticon.com/128/3342/3342207.png'  className='albumicon'  />
+                                    </div>
+                                    <h3 className='albumtitle' > ritesh </h3>
+                              </li>
+                              <li className='albums' >
+                                    <div>
+                                    <img  src='https://cdn-icons-png.flaticon.com/128/3342/3342207.png'  className='albumicon'  />
+                                    </div>
+                                    <h3 className='albumtitle' > ritesh </h3>
+                              </li>
+                              <li className='albums' >
+                                    <div>
+                                    <img  src='https://cdn-icons-png.flaticon.com/128/3342/3342207.png'  className='albumicon'  />
+                                    </div>
+                                    <h3 className='albumtitle' > ritesh </h3>
+                              </li>
+                              <li className='albums' >
+                                    <div>
+                                    <img  src='https://cdn-icons-png.flaticon.com/128/3342/3342207.png'  className='albumicon'  />
+                                    </div>
+                                    <h3 className='albumtitle' > ritesh </h3>
+                              </li>
+                              <li className='albums' >
+                                    <div>
+                                    <img  src='https://cdn-icons-png.flaticon.com/128/3342/3342207.png'  className='albumicon'  />
+                                    </div>
+                                    <h3 className='albumtitle' > ritesh </h3>
+                              </li>
+                              
+                        </ul>
+                  </div>
+            </>
+      )
 }
