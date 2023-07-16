db07fbb6e1210c18c36b5a944953fecb388c7981
riteshray007 <ray.ritesh007@gmail.com>
2023-07-16 13:14:39 +0530

    navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+      return (
+            <div className='navbarmaindiv' >
+                  <img className='navicon' src='https://cdn-icons-png.flaticon.com/128/2489/2489706.png' />
+                  <h1 className='navtitle' > MomentBook </h1>      
+            </div>
+      )
+}
